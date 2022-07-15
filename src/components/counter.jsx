a2ddbb5192be74bbf45fbd2be61d24ab785b97c1
Tamilarasan Annamalai <tamilarasanars@gmail.com>
2022-07-15 11:34:00 +0530

    Apply the style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        // imageUrl:'https://picsum.photos/200'
+    }
+    styles = {
+        fontSize:20,
+        fontWeight:'bold'
     }
 
     render() { 
         return (
               <React.Fragment>
-                <span>{this.formateCount()}</span>
-                <button >Increment</button>
+                {/* <img src={this.state.imageUrl} alt=""></img> */}
+                <span style={this.styles}  className="badge badge-primary m-2">{this.formateCount()}</span>
+                <button className='btn btn-secondary btn-sm'>Increment</button>
               </React.Fragment>
         );  
     }
 
     formateCount () {
         const {count}  = this.state;
-        return count === 0 ? <h1>Zero</h1> : count;
+        return count === 0 ? "Zero" : count;
     }
 }
  
