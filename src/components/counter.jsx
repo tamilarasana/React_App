276aee225052e2cd53c1fcde12240befe861ea0f
Tamilarasan Annamalai <tamilarasanars@gmail.com>
2022-07-15 16:43:18 +0530

    passing Event Arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,53 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-        tags:['tag1','tag2','tag3']
-    };
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>There are no Tags!</p>;
-        return <ul>{this.state.tags.map(tag=><li key= {tag}>{tag}</li>)}</ul>;
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  // constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-    }
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
+  // handleIncrement  ()  {
+  //     console.log("Increment Clicked",this);
+  // }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
 
-    // handleIncrement  ()  {
-    //     console.log("Increment Clicked",this);
-    // }
-    handleIncrement  = () =>  {
-        this.setState({
-            count:this.state.count + 1
-        })
-    };
+  doHandleIncrement = () => {
+    this.handleIncrement({id:1});
+  }
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formateCount()}</span>
+        <button
+          onClick={this.doHandleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please Create a new Tag!"}
+        {this.renderTags()}
+      </div>
+    );
+  }
 
-    render() {        
-        return (
-              <div>
-                <span    className={this.getBadgeClasses()}>{this.formateCount()}</span>
-                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
-                {this.state.tags.length === 0 &&  "Please Create a new Tag!"} 
-                {this.renderTags() }
-              </div>
-        );  
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-
-    formateCount () {
-        const {count}  = this.state;
-        return count === 0 ? "Zero" : count;
-    }
-
-
+  formateCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
