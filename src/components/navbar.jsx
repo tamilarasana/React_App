80c7e45d521eb8f163bd0a53392cd14bcfbe453b
Tamilarasan Annamalai <tamilarasanars@gmail.com>
2022-07-22 10:27:54 +0530

    Lifiting  the State Up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,6 +7,7 @@ class NavBar extends Component {
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             Navbar
+            <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
           </a>
         </div>
       </nav>
