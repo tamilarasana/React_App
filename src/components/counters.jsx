ae17769c732db969a2ff76a91ff16aa2b5184e2f
Tamilarasan Annamalai <tamilarasanars@gmail.com>
2022-07-22 11:18:42 +0530

    Destructuing Arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-
   render() {
+    const {onReset,counters,onIncrement,onDelete} = this.props;
     return (
       <div>
-        <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Rest</button>
-        {this.props.counters.map((counter) => (
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">Rest</button>
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
             counter={counter}
           ></Counter>
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,13 +15,13 @@ import React, { Component } from "react";
 //   }
 // }
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
   return (
     <nav className="navbar bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Navbar
-          <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+          <span className="badge badge-pill badge-secondary">{totalCounters}</span>
         </a>
       </div>
     </nav>
